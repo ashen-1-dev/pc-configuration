aaa5c97331176272e1679373cb2cce079013dd9f
ashen-1-dev <alen2014@gmail.com>
2022-03-30 17:58:33 +0700

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Hash;
+
+class UserController extends Controller
+{
+
+    public function register(Request $request): \Illuminate\Http\JsonResponse
+    {
+        if(User::where('email', '=', $request->email)->first() != null)
+            return response()->json(
+                [
+                    'success' => false,
+                    'data' => 'User already exist'
+                ]
+            );
+        $data = $request->validate(
+            [
+                'first_name' => ['required'],
+                'last_name' => ['required'],
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]);
+        $data['password'] = Hash::make($request->password);
+        $user = User::create($data);
+        $user->save();
+        return response()->json(
+            [
+                'success' => true,
+                'data' => $data,
+            ], 201);
+    }
+
+    public function login(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $credentials = $request->validate(
+            [
+                'email' => ['required', 'email'],
+                'password' => ['required'],
+            ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = User::where('email', '=', $credentials['email'])->first();
+            Auth::login($user);
+            return response()->json(
+                [
+                    'success' => true,
+                    'data' => 'Success login'
+                ]);
+        }
+
+        return response()->json(
+            [
+                'success' => false,
+                'data' => 'Failed login'
+            ]);
+    }
+}
